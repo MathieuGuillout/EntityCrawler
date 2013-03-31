bccda90de10ec4b4cd75c53fc2cbc4b021fdf795
MathieuGuillout <mathieu@creativelicence.com.au>
2013-03-31 17:00:50 +1100

    Add constant value

--- lib/stylesheet.rb
+++ lib/stylesheet.rb
@@ -6,5 +6,17 @@ class Stylesheet
 
   def initialize path
     @style = Helper.hashes_to_ostruct(YAML.load_file(path))
+    if @style.site.inherits
+      parent_path = @style.site.inherits
+      parent_path += ".yaml" unless parent_path.match /\.yaml$/
+      parent_path = path.gsub /[^\/]+$/, parent_path
+      parent_style = Stylesheet.new parent_path 
+      p parent_style
+      # Take parent attributes
+      # Default string => Selector for attributes
+      # Merge with parent
+      # Implement postprocessors
+    end
   end
+
 end
