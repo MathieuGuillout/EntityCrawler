c073706af33d627cad5ede50226eb937a452ddae
MathieuGuillout <mathieu@creativelicence.com.au>
2013-08-07 17:17:52 +1000

    Smaller buffer

--- lib/job.rb
+++ lib/job.rb
@@ -7,7 +7,7 @@ require_relative "crawler"
 require_relative "cdn"
 require_relative "helper"
 
-NB_MAX_ENTITIES_PER_CRAWL = 100
+NB_MAX_ENTITIES_PER_CRAWL = 20
 
 class Job
   attr_reader :entities, 
