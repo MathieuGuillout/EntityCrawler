580d92496f184a51309a3c4c3d4742cf6a19b427
MathieuGuillout <mathieu@creativelicence.com.au>
2013-09-05 16:25:34 +1000

    Maximum depth level

--- lib/job.rb
+++ lib/job.rb
@@ -62,7 +62,6 @@ class Job
         link[:crawl_timestamp] = @crawl_timestamp
         details = Helper.hostruct(link)
         job = JobDescription.new(details.url, @site_name, link[:type])
-        job.level = link[:type] == "site" ? 0 : 1
         jobs << job
       end
     end
@@ -184,5 +183,6 @@ class Job
   def clean
     @entities = []
     @new_jobs = []
+    @details = nil
   end
 end
